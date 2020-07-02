d4928dd4115459561b973ec25999658549ccfeeb
minji-o-j <jminji98@gmail.com>
2020-07-02 18:34:08 +0900

    C1C2

---
+++ src/C1C2.sol
@@ -0,0 +1,35 @@
+//pragma solidity 0.5.1;
+pragma solidity 0.4.21;
+contract C1 {
+    uint128 v1;
+    
+    function set(uint128 _v1) public {
+        v1=_v1;
+    }
+    function get() public view returns(uint128) {
+        return v1;
+    }
+    function get7() public pure returns(uint128) {
+        return 7;
+    }
+}
+
+contract C2 {
+    C1 c1;
+    function C2() public {  //0.4.21 constructor
+    //constructor() public {    //0.6 constructor
+        c1=new C1();
+    }
+    function set(uint128 _v1) public {
+        c1.set(_v1);
+    }
+    function get() public view returns(uint128) {
+        return c1.get();
+    }
+    function get7() public view returns(uint128) {
+        return c1.get7();
+    }
+    function getC1Address() public view returns(address) {
+        return address(c1);
+    }
+}
